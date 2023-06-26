77d37e1629d3bb44cac030474d76609bfab25011
Erdinc Mutlu <erdincmutlu@gmail.com>
2023-06-26 14:52:08 +0100

    Create serializer for Room

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -16,5 +16,5 @@ class Room(models.Model):
     code = models.CharField(max_length=8, default="", unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
-    votes_skip = models.IntegerField(null=False, default=1)
+    votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
